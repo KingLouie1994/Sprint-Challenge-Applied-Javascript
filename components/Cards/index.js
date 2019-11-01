ecc3284259613ce1a75993f5e881e59275fc7e95
Luis Schekerka <luis@Luiss-MBP.fritz.box>
2019-11-01 10:29:41 +0100

    added article cards to the dom

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function articleCreator(data) {
+    let card = document.createElement('div');
+    let headline = document.createElement('div');
+    let author = document.createElement('div');
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('img');
+    let authorsName = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = data.headline;
+    img.src = data.authorPhoto;
+    authorsName = data.authorName;
+
+    card.append(headline);
+    card.append(author);
+    author.append(imgContainer);
+    imgContainer.append(img);
+    author.append(authorsName);
+
+    return card;
+}
+
+const cardContainer = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        Object.keys(response.data.articles).forEach(event => {
+            response.data.articles[event].forEach(event2 => {
+                cardContainer.append(articleCreator(event2));
+            })
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
